548c6dbcb03646c8fae674e9868194eb0e535160
SunnykkoD <82902388+SunnykkoD@users.noreply.github.com>
2021-10-03 02:09:54 -0400

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -6,7 +6,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Compress Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\nSupport Group: @UniversalBotsSupport"
+    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Compress Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details."
    
     ABS_TEXT = " Please don't be selfish."
     
@@ -17,15 +17,15 @@ class Localisation:
     
     UPLOAD_START = "📤 Uploading ... 📤 \n"
     
-    COMPRESS_START = "📀 Trying to compress ... 📀"
+    COMPRESS_START = "🎬 Trying to compress ... 🎬"
     
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy @UniversalBotsUpdate"
+    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n🎬 Compressed in {}\n\n📤 Uploaded in {}"
 
-    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+    COMPRESS_PROGRESS = "⏳ ETA: {}\n📊 Progress: {}%"
 
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @UniversalBotsSupport"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress`"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
